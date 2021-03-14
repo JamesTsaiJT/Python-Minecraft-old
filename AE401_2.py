d883515a2d6689beedbac81a59ca29384dd8f8df
JamesTsaiJT <43771177+JamesTsaiJT@users.noreply.github.com>
2021-03-14 21:23:39 +0800

    Update AE401_2.py

--- AE401_2.py
+++ AE401_2.py
@@ -6,7 +6,7 @@ Created on Sun Mar 14 20:34:02 2021
 """
 
 from mcpi.minecraft import Minecraft
-
+import time
 mc = Minecraft.create()
 """
 t = 0
@@ -15,8 +15,24 @@ while True:
     t = t+1
     mc.postToChat("第"+str(t)+"次")
 """
-import time
 
+x = -116
+y = 67
+z = -144
+x = x+1
+z = z+1
+mc.player.setTilePos(x,y,z)
+time.sleep(0.5)
+x = x+1
+z = z+1
+mc.player.setTilePos(x,y,z)
+time.sleep(0.5)
+x = x+1
+z = z+1
+mc.player.setTilePos(x,y,z)
+time.sleep(0.5)
+
+"""
 mc.postToChat("I'm watching you.")
 
 while True:
@@ -25,3 +41,4 @@ while True:
                   "Y:"+str(y)+
                   "Z:"+str(z))
     time.sleep(0.5)
+"""
